2c2b73473e04b04babce36f274557a6b7e5a2e1a
lmrangeljr <150730776+lmrangeljr@users.noreply.github.com>
2024-02-26 09:24:42 -0500

    Update A_PROCEDURES.sql
    Procedure code

--- A_PROCEDURES.sql
+++ A_PROCEDURES.sql
@@ -33,98 +33,69 @@ SELECT MAIN.PATIENT_ID, MAIN.INDEX_DATE, 'FU_6MO' AS RUN_ID FROM RESEARCH.GSK_CR
 ********************************************************************************************************************
 * CUIs
 ********************************************************************************************************************
-* 		boc009098		*		Sinus diagnostic endoscopy (S)
-* 		boc004289		*		Nasal Sinus Computed Tomography (CT) (S)
-* 		boc009100		*		Nasal Sinus MRI (S)
-* 		boc004249		*		Functional endoscopic sinus surgery (FESS)
-* 		boc004267		*		Nasal polypectomy (S)
+*               boc009098               *               Sinus diagnostic endoscopy (S)
+*               boc004289               *               Nasal Sinus Computed Tomography (CT) (S)
+*               boc009100               *               Nasal Sinus MRI (S)
+*               boc004249               *               Functional endoscopic sinus surgery (FESS)
+*               boc004267               *               Nasal polypectomy (S)
 * 
 ********************************************************************************************************************
 * CPT/HCPCS 
 ********************************************************************************************************************
-*		31237			*		Nasal/sinus endoscopy, surgical; with biopsy, polypectomy or debridement (separate procedure)
-* 		S2342			*		Nasal endoscopy for postoperative debridement following functional endoscopic sinus surgery, nasal and/or sinus cavity(s), unilateral or bilateral
+*               31237                   *               Nasal/sinus endoscopy, surgical; with biopsy, polypectomy or debridement (separate procedure)
+*               S2342                   *               Nasal endoscopy for postoperative debridement following functional endoscopic sinus surgery, nasal and/or sinus cavity(s), unilateral or bilateral
 *******************************************************************************************************************/
-CREATE OR REPLACE TEMPORARY TABLE PT1 AS
-WITH CUIS AS (SELECT DISTINCT A.PROVIDER_ID
-					,b.SPECIALTY_CUI
-			FROM DELIVERED_211.profile_store.provider_specialty AS A
-			INNER JOIN DELIVERED_211.profile_store.specialty AS B ON A.specialty_id = B.specialty_id
-			WHERE b.SPECIALTY_CUI IN('boc002384','boc002358','boc002359'))
-,DX_CUIS as (select distinct a.code_type as code_type_cui
-					,A.mapped_diagnosis_code
-					,A.boc_cui
-					,A.boc_name
-					,B.boc_name as code_type_name
-			FROM MAPSET_20231004.public.boc_map_diagnosis_rollup a 
-			INNER JOIN MAPSET_20231004.public.boc_map_diagnosis_code_type b on a.code_type = b.boc_cui
-			WHERE a.boc_cui in('boc004248','boc004265'))
-,ALL_DX as (SELECT DISTINCT MAIN.PATIENT_ID
-					,A.ENCOUNTER_ID
-					,TO_DATE(A.DIAGNOSIS_DATE) AS DX_DATE
-					,B.boc_cui
-			FROM RESEARCH.GSK_CRSWNP.A_PATIENT_LMRJR AS MAIN
-			INNER JOIN DELIVERED_211.profile_store.diagnosis AS A ON MAIN.PATIENT_ID=A.PATIENT_ID
-			INNER JOIN DX_CUIS b ON (trim(lower(a.code)) = trim(lower(b.mapped_diagnosis_code)) AND trim(lower(a.code_type_name)) = trim(lower(b.code_type_name))))
-,ENC_DATA AS (SELECT MAIN.PATIENT_ID
-					,BASE.ENCOUNTER_ID
-					,PROV.PROVIDER_ID
-					,MM.ENCOUNTER_TYPE
-					,TO_DATE(MM.START_DATE) AS ENC_DATE
-			FROM RESEARCH.GSK_CRSWNP.A_PATIENT_LMRJR AS MAIN
-			JOIN DELIVERED_211.PROFILE_STORE.CLINICAL_ENCOUNTER AS MM ON MAIN.PATIENT_ID=MM.PATIENT_ID
-			JOIN DELIVERED_211.PROFILE_STORE.CLINICAL_ENCOUNTER_LINK AS BASE ON MAIN.PATIENT_ID=BASE.PATIENT_ID AND MM.CLINICAL_ENCOUNTER_ID=BASE.CLINICAL_ENCOUNTER_ID
-			JOIN DELIVERED_211.PROFILE_STORE.ENCOUNTER_PROVIDER AS PROV ON BASE.ENCOUNTER_ID=PROV.ENCOUNTER_ID
-			WHERE TO_DATE(MM.START_DATE) BETWEEN DATEADD(DAY, -12*30.4375, MAIN.INDEX_DATE) AND DATEADD(DAY, 6*30.4375, (DATEADD(DAY, 1, MAIN.INDEX_DATE)))
-					AND LOWER(MM.ENCOUNTER_TYPE) IN('outpatient','ed'))
-,TT AS (SELECT DISTINCT ED.PATIENT_ID
-				,ED.ENC_DATE
-				,CAT.SPECIALTY_CUI
-				,DXD.BOC_CUI
-				,DX_DATE
-				,ENCOUNTER_TYPE
-				,case when ABS(DATEDIFF(DAY, DXD.DX_DATE, ED.ENC_DATE)) <= 5 then 1 else 0 end as within5days
-		  FROM ENC_DATA AS ED
-		  LEFT JOIN CUIS AS CAT ON ED.PROVIDER_ID=CAT.PROVIDER_ID
-		  LEFT JOIN ALL_DX AS DXD ON ED.PATIENT_ID=DXD.PATIENT_ID)
-SELECT DISTINCT MAIN.PATIENT_ID
-		,MAIN.INDEX_DATE
+CREATE OR REPLACE TEMPORARY TABLE R_COHORT AS
+SELECT * FROM RESEARCH.GSK_CRSWNP.A_PATIENT_LMRJR;
+
+-- GETTING MY PROCEDURES
+CREATE OR REPLACE TEMPORARY TABLE MY_PROCS AS
+WITH ALL_PROCS AS (
+		SELECT MAIN.PATIENT_ID
+				,TO_DATE(MAIN.procedure_date) AS PROC_DATE
+				,BASE.boc_cui
+		FROM profile_store.procedure AS MAIN
+		JOIN mapset_20220817.public.boc_map_procedure_rollup AS BASE ON MAIN.code=BASE.mapped_procedure_code AND MAIN.code_type_cui=BASE.code_type
+		WHERE BASE.boc_cui IN ('boc009098','boc004289','boc009100','boc004249','boc004267'))
+SELECT MAIN.patient_id
+		,MAIN.index_date
 		,CASE
-				WHEN BASE.ENC_DATE IS NOT NULL AND (DATEADD(DAY, -12*30.4375, MAIN.INDEX_DATE) <= BASE.ENC_DATE AND BASE.ENC_DATE < DATEADD(DAY, -6*30.4375, MAIN.INDEX_DATE)) THEN 'BL_12MO'
-				WHEN BASE.ENC_DATE IS NOT NULL AND (DATEADD(DAY, -6*30.4375, MAIN.INDEX_DATE) <= BASE.ENC_DATE AND BASE.ENC_DATE <= MAIN.INDEX_DATE) THEN 'BL_6MO'
-				WHEN BASE.ENC_DATE IS NOT NULL AND (MAIN.INDEX_DATE < BASE.ENC_DATE AND BASE.ENC_DATE <= DATEADD(DAY, 6*30.4375, (DATEADD(DAY, 1, MAIN.INDEX_DATE)))) THEN 'FU_6MO'
+				WHEN BASE.PROC_DATE IS NOT NULL AND (DATEADD(DAY, -12*30.4375, MAIN.INDEX_DATE) <= BASE.PROC_DATE AND BASE.PROC_DATE < DATEADD(DAY, -6*30.4375, MAIN.INDEX_DATE)) THEN 'BL_12MO'
+				WHEN BASE.PROC_DATE IS NOT NULL AND (DATEADD(DAY, -6*30.4375, MAIN.INDEX_DATE) <= BASE.PROC_DATE AND BASE.PROC_DATE <= MAIN.INDEX_DATE) THEN 'BL_6MO'
+				WHEN BASE.PROC_DATE IS NOT NULL AND (MAIN.INDEX_DATE < BASE.PROC_DATE AND BASE.PROC_DATE <= DATEADD(DAY, 6*30.4375, (DATEADD(DAY, 1, MAIN.INDEX_DATE)))) THEN 'FU_6MO'
 				ELSE 'NO DATA'
 		END AS RUN_ID
-	,count(distinct CASE WHEN LOWER(BASE.ENCOUNTER_TYPE) = 'outpatient' THEN ENC_DATE ELSE null END) AS OP_visit
-		,count(distinct CASE WHEN LOWER(BASE.ENCOUNTER_TYPE) = 'ed' THEN ENC_DATE ELSE null END) AS ED_visit
-		,count(distinct CASE WHEN LOWER(BASE.ENCOUNTER_TYPE) = 'outpatient' and BASE.SPECIALTY_CUI = 'boc002384' THEN ENC_DATE ELSE null END) AS PLMG_visit
-		,count(distinct CASE WHEN LOWER(BASE.ENCOUNTER_TYPE) = 'outpatient' and BASE.SPECIALTY_CUI = 'boc002358' THEN ENC_DATE ELSE null END) AS ALGY_visit
-		,count(distinct CASE WHEN LOWER(BASE.ENCOUNTER_TYPE) = 'outpatient' and BASE.SPECIALTY_CUI = 'boc002359' THEN ENC_DATE ELSE null END) AS ORL_visit
-		,count(distinct CASE WHEN LOWER(BASE.ENCOUNTER_TYPE) = 'outpatient' and BASE.SPECIALTY_CUI = 'boc002384' and within5days = 1 THEN ENC_DATE ELSE null END) AS CRSwNP_PLMG_visit
-		,count(distinct CASE WHEN LOWER(BASE.ENCOUNTER_TYPE) = 'outpatient' and BASE.SPECIALTY_CUI = 'boc002358' and within5days = 1 THEN ENC_DATE ELSE null END) AS CRSwNP_ALGY_visit
-		,count(distinct CASE WHEN LOWER(BASE.ENCOUNTER_TYPE) = 'outpatient' and BASE.SPECIALTY_CUI = 'boc002359' and within5days = 1 THEN ENC_DATE ELSE null END) AS CRSwNP_ORL_visit
-		,count(distinct CASE WHEN LOWER(BASE.ENCOUNTER_TYPE) = 'outpatient' and within5days = 1 THEN ENC_DATE ELSE null END) AS CRSwNP_OP_visit
-		,count(distinct CASE WHEN LOWER(BASE.ENCOUNTER_TYPE) = 'ed' and within5days = 1 THEN ENC_DATE ELSE null END) AS CRSwNP_ED_visit
-FROM RESEARCH.GSK_CRSWNP.A_PATIENT_LMRJR AS MAIN
-LEFT JOIN TT AS BASE ON MAIN.PATIENT_ID=BASE.PATIENT_ID
-GROUP BY 1,2,3;
+		,COUNT(DISTINCT CASE WHEN BASE.BOC_CUI = 'boc009098' THEN BASE.PROC_DATE ELSE NULL END) AS NASAL_ENDO
+		,COUNT(DISTINCT CASE WHEN BASE.BOC_CUI = 'boc004289' THEN BASE.PROC_DATE ELSE NULL END) AS SINUS_CT
+		,COUNT(DISTINCT CASE WHEN BASE.BOC_CUI = 'boc009100' THEN BASE.PROC_DATE ELSE NULL END) AS SINUS_MRI
+		,COUNT(DISTINCT CASE WHEN BASE.BOC_CUI = 'boc004249' THEN BASE.PROC_DATE ELSE NULL END) AS ESS
+		,COUNT(DISTINCT CASE WHEN BASE.BOC_CUI = 'boc004267' THEN BASE.PROC_DATE ELSE NULL END) AS POLYPEC
+FROM R_COHORT AS MAIN
+LEFT JOIN ALL_PROCS AS BASE ON MAIN.patient_id = BASE.patient_id
+GROUP BY 1, 2, 3;
 
 /*******************************************************************************************************************
 * PUTTING IT ALL TOGETHER
 *******************************************************************************************************************/
-CREATE OR REPLACE TABLE RESEARCH.GSK_CRSWNP.A_VISITS_LMRJR AS
+CREATE OR REPLACE TABLE RESEARCH.GSK_CRSWNP.A_PROCEDURES_LMRJR AS
 SELECT MAIN.PATIENT_ID
-	,MAIN.INDEX_DATE
-	,MAIN.RUN_ID
-	,CASE WHEN FTA.PLMG_visit IS NULL THEN 0 ELSE FTA.PLMG_visit END AS PLMG_visit
-	,CASE WHEN FTA.ALGY_visit IS NULL THEN 0 ELSE FTA.ALGY_visit END AS ALGY_visit
-	,CASE WHEN FTA.ORL_visit IS NULL THEN 0 ELSE FTA.ORL_visit END AS ORL_visit
-	,CASE WHEN FTA.OP_visit IS NULL THEN 0 ELSE FTA.OP_visit END AS OP_visit
-	,CASE WHEN FTA.ED_visit IS NULL THEN 0 ELSE FTA.ED_visit END AS ED_visit
-	,CASE WHEN FTA.CRSwNP_PLMG_visit IS NULL THEN 0 ELSE FTA.CRSwNP_PLMG_visit END AS CRSwNP_PLMG_visit
-	,CASE WHEN FTA.CRSwNP_ALGY_visit IS NULL THEN 0 ELSE FTA.CRSwNP_ALGY_visit END AS CRSwNP_ALGY_visit
-	,CASE WHEN FTA.CRSwNP_ORL_visit IS NULL THEN 0 ELSE FTA.CRSwNP_ORL_visit END AS CRSwNP_ORL_visit
-	,CASE WHEN FTA.CRSwNP_OP_visit IS NULL THEN 0 ELSE FTA.CRSwNP_OP_visit END AS CRSwNP_OP_visit
-	,CASE WHEN FTA.CRSwNP_ED_visit IS NULL THEN 0 ELSE FTA.CRSwNP_ED_visit END AS CRSwNP_ED_visit
+		,MAIN.INDEX_DATE
+		,MAIN.RUN_ID
+		,CASE WHEN FTA.NASAL_ENDO IS NULL THEN 0 ELSE FTA.NASAL_ENDO END AS NASAL_ENDO
+		,CASE WHEN FTA.SINUS_CT IS NULL THEN 0 ELSE FTA.SINUS_CT END AS SINUS_CT
+		,CASE WHEN FTA.SINUS_MRI IS NULL THEN 0 ELSE FTA.SINUS_MRI END AS SINUS_MRI
+		,CASE WHEN FTA.ESS IS NULL THEN 0 ELSE FTA.ESS END AS ESS
+		,CASE WHEN FTA.POLYPEC IS NULL THEN 0 ELSE FTA.POLYPEC END AS POLYPEC
 FROM FINAL_TABLE AS MAIN
-LEFT JOIN PT1 AS FTA ON MAIN.PATIENT_ID=FTA.PATIENT_ID AND MAIN.INDEX_DATE=FTA.INDEX_DATE AND MAIN.RUN_ID=FTA.RUN_ID;
+LEFT JOIN MY_PROCS AS FTA ON MAIN.PATIENT_ID=FTA.PATIENT_ID AND MAIN.INDEX_DATE=FTA.INDEX_DATE AND MAIN.RUN_ID=FTA.RUN_ID;
+
+
+
+SELECT COUNT(1) AS ROW_CNT
+		,COUNT(DISTINCT PATIENT_ID) AS PAT_CNT
+FROM MY_PROCS;
+
+SELECT *
+FROM MY_PROCS
+LIMIT 100;
+
